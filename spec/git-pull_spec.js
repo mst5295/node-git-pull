cf94b5d4991b95082c0ae44ea432ae5bb7d8157b
Meike Steinleger <Meike.steinleger@t-online.de>
2017-10-16 15:06:24 +0200

    add jasmine for testing

---
+++ spec/git-pull_spec.js
@@ -0,0 +1,15 @@
+var pull = require('../git-pull.js'),
+    link = "",
+    local = "";
+const { createFetch, createStack, enableRecv, header, base, parse } = require('http-client');
+
+describe('Git Pull', function(){
+    describe('pulls a repo on Github', function(){
+        it("returns a code = 0 and echo no message", function(done){
+
+        });
+        it("returns a code != 0 and echo a message" , function(done){
+            
+        });
+    })
+});
