2a0d720c363a0d5d170f2785fe9592c66cc1c62d
Meike Steinleger <Meike.steinleger@t-online.de>
2017-10-16 14:17:07 +0200

    add shelljs and call git pull

---
+++ git-pull.js
@@ -0,0 +1,8 @@
+var shell = require('shelljs');
+
+module.exports = function(link, local){
+    if(shell.exec('git pull ' + link + ' ' + local).code !== 0){
+        shell.echo('Error: Git pull failed');
+        shell.exit(1);
+    }
+}
