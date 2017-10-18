5517c20903f0dbe87c8a38bcc60c704ebcf34587
Unknown <Meike.steinleger@t-online.de>
2017-10-18 12:30:02 +0200

    change test

--- git-pull.js
+++ git-pull.js
@@ -4,9 +4,5 @@ module.exports = function(link, local){
     if(shell.exec('git pull ' + link + ' '+ local).code !== 0){
         shell.echo('Error: Git pull failed');
         shell.exit(1);
-        return 1;
-    }else{
-        shell.exit(1);
-        return 0;
     }
 }
--- spec/git-pull-spec.js
+++ spec/git-pull-spec.js
@@ -6,11 +6,11 @@ local = "./";
 describe('Git Pull', function(){
 describe('pulls a repo on Github', function(){
     it("returns a code = 0", function(done){
-        expect(pull(link_rigth, local)).toBe(0);
+        expect(pull(link_rigth, local).code).toBe(0);
         done;
     });
     it("returns a code != 0" , function(done){
-        expect(pull(link_false, local)).isNot(0);
+        expect(pull(link_false, local).code).isNot(0);
         done;
     });
 })
