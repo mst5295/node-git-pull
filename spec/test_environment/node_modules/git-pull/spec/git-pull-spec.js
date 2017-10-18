37aafd99749dde3ee36b3f3e989d0eaae2296214
Meike Steinleger <Meike.steinleger@t-online.de>
2017-10-18 12:16:13 +0200

    install git-pull-node in docker

---
+++ spec/test_environment/node_modules/git-pull/spec/git-pull-spec.js
@@ -0,0 +1,17 @@
+var pull = require('../git-pull.js'),
+link_rigth = "https://github.com/mst5295/testrepo_gitPull",
+link_false = "https://github.com/mst5295/testrepogitPull",
+local = "./";
+
+describe('Git Pull', function(){
+describe('pulls a repo on Github', function(){
+    it("returns a code = 0", function(done){
+        expect(pull(link_rigth, local)).toBe(0);
+        done;
+    });
+    it("returns a code != 0" , function(done){
+        expect(pull(link_false, local)).isNot(0);
+        done;
+    });
+})
+});
