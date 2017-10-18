3e08e404f99e64cd499ce39c42859b522ad48644
Meike Steinleger <Meike.steinleger@t-online.de>
2017-10-18 14:12:05 +0200

    change test

--- spec/git-pull-spec.js
+++ spec/git-pull-spec.js
@@ -1,16 +1,16 @@
 var pull = require('../git-pull.js'),
 link_rigth = "https://github.com/mst5295/testrepo_gitPull.git",
 link_false = "https://github.com/mst5295/testrepogitPull.git",
-local = "./";
+branch = "";
 
 describe('Git Pull', function(){
 describe('pulls a repo on Github', function(){
     it("returns a code = 0", function(done){
-        expect(pull(link_rigth, local).code).toBe(0);
+        expect(pull(link_rigth, branch).code).toBe(0);
         done;
     });
     it("returns a code != 0" , function(done){
-        expect(pull(link_false, local)).isNot(0);
+        expect(pull(link_false, branch)).isNot(0);
         done;
     });
 })
