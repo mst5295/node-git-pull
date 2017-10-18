301c65fde1298995f8ad5bff7346cf994fd3e58b
Meike Steinleger <Meike.steinleger@t-online.de>
2017-10-18 15:48:05 +0200

    change test

--- git-pull.js
+++ git-pull.js
@@ -1,7 +1,7 @@
 var shell = require('shelljs');
 
-module.exports = function(link, branch){
-    if(shell.exec('git pull ' + link + ' ' + branch).code !== 0){
+module.exports = function(link, branch, local){
+    if(shell.exec('git --work-tree='+ local +' pull '+ link + ' ' + branch ).code !== 0){
         shell.echo('Error: Git pull failed');
         shell.exit(1);
     }
--- spec/git-pull-spec.js
+++ spec/git-pull-spec.js
@@ -1,16 +1,17 @@
 var pull = require('../git-pull.js'),
 link_rigth = "https://github.com/mst5295/testrepo_gitPull.git",
 link_false = "https://github.com/mst5295/testrepogitPull.git",
-branch = "";
+branch = "",
+local = "../restrepo_gitPull/";
 
 describe('Git Pull', function(){
 describe('pulls a repo on Github', function(){
     it("returns a code = 0", function(done){
-        expect(pull(link_rigth, branch).code).toBe(0);
+        expect(pull(link_rigth, branch, local).code).toBe(0);
         done;
     });
     it("returns a code != 0" , function(done){
-        expect(pull(link_false, branch)).isNot(0);
+        expect(pull(link_false, branch, local)).isNot(0);
         done;
     });
 })
