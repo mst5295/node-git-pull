00467d11967b9666255db15042d395538f970c67
Meike Steinleger <Meike.steinleger@t-online.de>
2017-10-19 14:59:27 +0200

    refactor git-pull, git-pull-spec

--- git-pull.js
+++ git-pull.js
@@ -1,7 +1,51 @@
 var shell = require('shelljs'),
     path = require('path'),
     fs = require('fs');
+
 const NEEDS_PULL=0, NEEDS_CLONE=1;
+
+function generateAbsPath(local){
+    var localAbsPath;
+    if(path.isAbsolute(local)){
+        localAbsPath = local;
+    }else{
+        localAbsPath = path.resolve(local);
+    }
+    return localAbsPath;
+}
+
+function checkPath(localAbsPath){
+    var localAbsPathGit = path.join(localAbsPath, '.git');
+    if(!fs.existsSync(localAbsPath)){
+        shell.mkdir(localAbsPath);
+        return NEEDS_CLONE;
+    }else if(!fs.statSync(localAbsPath).isDirectory()){
+        throw(new Error ('Error: Absolute path is not a directory'));
+    }else if(!fs.statSync(localAbsPathGit).isDirectory()){
+        throw(new Error('Error: Absolute path is no git-repo'))
+    }
+    return NEEDS_PULL;
+}
+
+function gitPull(localAbsPath, branch){
+    shell.cd(localAbsPath);
+    var retCode = shell.exec('git pull ' + branch + ' --allow-unrelated-histories').code;
+    if( retCode !== 0){
+        shell.exit(1);
+        throw(new Error('Error: Git pull failed'));
+    }
+    return retCode;
+}
+
+function gitClone(localAbsPath, branch, link){
+    shell.cd(localAbsPath);
+    var retCode = shell.exec('git clone '+link+ ' ' + branch).code;
+    if( retCode !== 0){
+        shell.exit(1);
+        throw(new Error('Error: Git clone failed'));
+    }
+}
+
 module.exports = function(link, branch, local){
     var localAbsPath,
         gitEvent;
@@ -9,49 +53,11 @@ module.exports = function(link, branch, local){
         localAbsPath = generateAbsPath(local);
         gitEvent = checkPath(localAbsPath);
         if(gitEvent == NEEDS_PULL){
-            gitPull(localAbsPath, branch);
+            return gitPull(localAbsPath, branch);
         }else if(gitEvent == NEEDS_CLONE){
-            gitClone(localAbsPath, branch, link);
+            return gitClone(localAbsPath, branch, link);
         } else{
             throw(new Error('not a known git-event'));
         }
 
-
-    function generateAbsPath(local){
-        var localAbsPath;
-        if(path.isAbsolute(local)){
-            localAbsPath = local;
-        }else{
-            localAbsPath = path.resolve(local);
-        }
-        return localAbsPath;
-    }
-    function checkPath(localAbsPath){
-        var localAbsPathGit = path.join(localAbsPath, '.git');
-        if(!fs.existsSync(localAbsPath)){
-            shell.mkdir(localAbsPath);
-            return NEEDS_CLONE;
-        }else if(!fs.statSync(localAbsPath).isDirectory()){
-            throw(new Error ('Error: Absolute path is not a directory'));
-        }else if(!fs.statSync(localAbsPathGit).isDirectory()){
-            throw(new Error('Error: Absolute path is no git-repo'))
-        }
-        return NEEDS_PULL;
-    }
-
-    function gitPull(localAbsPath, branch){
-        shell.cd(localAbsPath);
-        if(shell.exec('git pull ' + branch + ' --allow-unrelated-histories').code !== 0){
-            shell.exit(1);
-            throw(new Error('Error: Git pull failed'));
-        }
-    }
-    function gitClone(localAbsPath, branch, link){
-        shell.cd(localAbsPath);
-        if(shell.exec('git clone '+link+ ' ' + branch).code !== 0){
-            shell.exit(1);
-            throw(new Error('Error: Git clone failed'));
-        }
-    }
-
-}
+};
--- spec/git-pull-spec.js
+++ spec/git-pull-spec.js
@@ -1,18 +1,28 @@
 var pull = require('../git-pull.js'),
-link_rigth = "https://github.com/mst5295/testrepo_gitPull",
-link_false = "https://github.com/mst5295/testrepogitPull",
-branch = "",
-local = "../testrepo_gitPull/";
+    shell = require('shelljs'),
+    link_rigth = "https://github.com/mst5295/testrepo_gitPull",
+    link_false = "https://github.com/mst5295/testrepogitPull",
+    branch = "",
+    local_pull = "./test_environment/assists/testrepo_gitPull_pull/",
+    local_clone = "./test_environment/assists/testrepo_gitPull_clone/";
 
 describe('Git Pull', function(){
-describe('pulls a repo on Github', function(){
-    it("returns a code = 0", function(done){
-        expect(pull(link_rigth, branch, local).code).toBe(0);
-        done;
-    });
-    /*it("returns a code != 0" , function(done){
-        expect(pull(link_false, branch, local)).isNot(0);
-        done;
-    });*/
-})
+    describe('clone a repo on Github',function(){
+        it("returns a code = 0 on success", function(done){
+            expect(pull(link_rigth, branch, local_clone)).toBe(0);
+            done();
+        });
+    })
+
+
+    describe('pulls a repo on Github', function(){
+        it("returns a code = 0 on success", function(done){
+            expect(pull(link_rigth, branch, local_pull)).toBe(0);
+            done();
+        });
+        /*it("returns a code != 0" , function(done){
+            expect(pull(link_false, branch, local)).isNot(0);
+            done();
+        });*/
+    })
 });
